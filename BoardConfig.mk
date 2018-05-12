fe4cbb704aade37d7e701dd828ac6d90c891a7e1
warrenlobo <warrenlobo360@gmail.com>
2018-05-12 23:28:12 +0800

    wt88047: Don't shim mm-qcamera-daemon

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,7 +80,6 @@ BOARD_SEPOLICY_DIRS += \
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
-    /system/vendor/bin/mm-qcamera-daemon|libshim_camera.so \
     /system/vendor/lib/libmmcamera2_imglib_modules.so|libshim_camera.so \
     /system/vendor/lib/lib-imsvt.so|libshims_ims.so
 
